dc1e39b27bc5f26325eae3e3f3a34d633ac8ceff
Archan6el <myk0675@outlook.com>
2025-10-29 13:01:46 -0500

    Changed the way server listens

--- webapp/app.js
+++ webapp/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 // Create express app
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
